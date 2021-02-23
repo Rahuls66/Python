df6b96de6d67d762c5ddf7a0aeb3fcce0f7afd66
Rahul Shah <rahul.shah4513@gmail.com>
2021-02-23 17:27:52 +0530

    Create Bonus2500.py
    Program to allot bonus of 2500/- if an employee has served more than 3 years in the company

---
+++ Bonus2500.py
@@ -0,0 +1,6 @@
+jnyr = int(input("Enter the year in which you joined this company: "))
+
+if 2021-jnyr > 3:
+    print("Congrats, You have got bonus of 2500!")
+else:
+  print("You did not got any bonus!")
