4928ed6472a63f90dcd6e25a30429d4cc7e03197
Rahul Shah <rahul.shah4513@gmail.com>
2021-02-27 11:49:39 +0530

    Update LeapYear.py
    Added expected output

--- LeapYear.py
+++ LeapYear.py
@@ -7,6 +7,20 @@ if year % 4 == 0:
         else:
             print("It's not a Leap Year")
     else:
-            print("It's not a Leap Year")
+            print("It's a Leap Year")
 else:
             print("It's not a Leap Year")
+'''
+Expected Output:
+Test Case I:
+Enter the year: 2020
+It's a Leap Year
+
+Test Case II:
+Enter the year: 2013
+It's not a Leap Year
+
+Test Case III:
+Enter the year: 1900
+It's not a Leap Year
+'''
