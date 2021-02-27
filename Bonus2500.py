a4d73156e490537702a61a0009009c0e936b4bee
Rahul Shah <rahul.shah4513@gmail.com>
2021-02-27 11:45:23 +0530

    Update Bonus2500.py
    Added expected output

--- Bonus2500.py
+++ Bonus2500.py
@@ -4,3 +4,14 @@ if 2021-jnyr > 3:
     print("Congrats, You have got bonus of 2500!")
 else:
   print("You did not got any bonus!")
+
+'''
+Expected Output:
+Test Case I:
+Enter the year in which you joined this company: 2018
+You did not got any bonus!
+
+Test Case II:
+Enter the year in which you joined this company: 2016
+Congratulations, You have got bonus of 2500!
+'''
