3f1f5132b797bb1af721d0b4f3dfc122859b153a
Rahul Shah <rahul.shah4513@gmail.com>
2021-02-27 11:53:16 +0530

    Update ProfitLoss.py
    Added expected output

--- ProfitLoss.py
+++ ProfitLoss.py
@@ -7,3 +7,24 @@ elif costp > sellp:
     print("\nProduct sold with Loss!")
 else:
     print("No Profit, No Loss!")
+
+'''
+Expected Output
+
+Test Case I:
+Enter the Cost Price for the Product: 12.22
+Enter the Selling Price for the Product: 34.68
+
+Product was sold with Profit!
+
+Test Case II:
+Enter the Cost Price for the Product: 54.97
+Enter the Selling Price for the Product: 23.478
+
+Product was sold with Profit!
+
+Test Case III:
+Enter the Cost Price for the Product: 55
+Enter the Selling Price for the Product: 55
+No Profit, No Loss!
+'''
