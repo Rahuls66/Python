f75fd23fe272f5db1cb1d74cd715039bba8adc35
Rahul Shah <rahul.shah4513@gmail.com>
2021-02-28 14:12:55 +0530

    Create TempFtoC.py

---
+++ TempFtoC.py
@@ -0,0 +1,14 @@
+f = 95
+#Accept User Input:
+#f = float(input("Enter Temperature in Fahrenheit: "))
+
+print("Entered Input for Temperature is: ", f, "°F")
+
+c = (f - 32) * 5/9
+print("Temperature in Fahrenheit is: ", c, "°C")
+
+'''
+Expected Output:
+Entered Input for Temperature is:  95 °F
+Temperature in Fahrenheit is:  35.0 °C
+'''
