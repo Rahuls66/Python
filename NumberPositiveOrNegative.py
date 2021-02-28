00c9675b315edab4f9a40fa6b6d6950ca4ec4159
Rahul Shah <rahul.shah4513@gmail.com>
2021-02-28 14:39:18 +0530

    Create NumberPositiveOrNegative.py

---
+++ NumberPositiveOrNegative.py
@@ -0,0 +1,24 @@
+#num = 24
+
+#Accept User Input:
+num = int(input("Enter Number: "))
+
+if num > 0:
+    print(num , "is a Positve Number!")
+elif num == 0:
+    print("Zero")
+else:
+    print(num, "is a Negative Number!")
+ 
+'''
+Expected Outpt:
+
+Enter Number: 37
+37 is a Positve Number!
+
+Enter Number: 0
+Zero
+
+Enter Number: -12
+-12 is a Negative Number!
+'''
