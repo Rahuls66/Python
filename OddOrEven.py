5337ce3472abb5d03c81164e6476f2d0c1a56cf8
Rahul Shah <rahul.shah4513@gmail.com>
2021-02-28 15:53:50 +0530

    Create OddOrEven.py

---
+++ OddOrEven.py
@@ -0,0 +1,20 @@
+num = 23
+
+#Accept User Input:
+num = int(input("Enter Number: "))
+
+if num % 2 == 0:
+    print("It's an Even Number")
+
+else:
+    print("It's a Odd Number")
+    
+'''
+Expected Output:
+
+Enter Number: 23
+It's a Odd Number
+
+Enter Number: 226
+It's an Even Number
+'''
