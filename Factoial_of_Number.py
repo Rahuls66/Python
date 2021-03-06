46ab69b1e3f4faf394cf5a9ed1cc6b8949d0c5dc
Rahul Shah <rahul.shah4513@gmail.com>
2021-03-07 00:01:54 +0530

    Improvised Program

--- Factoial_of_Number.py
+++ Factoial_of_Number.py
@@ -1,11 +1,14 @@
-#x = 7
-
 #Accept User Input
 x = int(input("Enter Number for which factorial to be calculated: "))
 factorial = 1
 
-for num in range(1 , x + 1):
-    factorial = factorial * num
+if x < 0:
+    factorial = 0
+elif x == 0 and x == 1:
+    factorial = 1
+else:
+    for num in range(1 , x + 1):
+        factorial = factorial * num
 
 print(x,"! is: ", factorial) 
 
@@ -13,5 +16,10 @@ print(x,"! is: ", factorial)
 Expected Output:
 Enter Number for which factorial is tobe calculated: 4
 4 ! is:  24
-24
+
+Enter Number for which factorial is tobe calculated: 0
+0 ! is:  1
+
+Enter Number for which factorial is tobe calculated: -1
+0 ! is:  0
 '''
