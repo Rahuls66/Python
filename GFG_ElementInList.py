184a215b2abea5c9e9fb041b0e403e4608dff112
Rahul Shah <rahul.shah4513@gmail.com>
2021-03-07 19:35:57 +0530

    Create GFG_ElementInList.py

---
+++ GFG_ElementInList.py
@@ -0,0 +1,10 @@
+def inList(x, list1):
+    if x in list1:
+        print(x, "exists in given collection.")
+
+inList(7, [1,2,3,4,5,6,7,8,9,10])
+
+'''
+Expected Output:
+7 exists in given collection.
+'''
