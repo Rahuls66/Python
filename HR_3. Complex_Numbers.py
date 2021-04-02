c1091029f2d2ff30494becf875f560766607a733
Rahul Shah <rahul.shah4513@gmail.com>
2021-04-02 23:17:09 +0530

    Create HR_3. Complex_Numbers.py

---
+++ HR_3. Complex_Numbers.py
@@ -0,0 +1,16 @@
+#Complex Numbers
+
+import cmath
+s = complex(input())
+#s = complex(s)
+r = abs(s)
+ph = cmath.phase(s)
+print(r)
+print(ph)
+
+'''
+Expected Output:
+1+2j
+2.23606797749979
+1.1071487177940904
+'''
