cb73020cdb895a2352e1dd25232b5fa7697ccdf7
Rahul Shah <rahul.shah4513@gmail.com>
2021-04-03 23:41:02 +0530

    Create HR_6. Unique_Elements.py

---
+++ HR_6. Unique_Elements.py
@@ -0,0 +1,23 @@
+#Unique Elements
+
+n = int(input())
+l1 = []
+for i in range(n):
+  k = str(input())
+  l1.append(k)
+
+s1 = set(l1)
+print(len(s1))
+
+'''
+Expected Output:
+7
+UK
+China
+USA
+France
+New Zealand
+UK
+France
+5
+'''
