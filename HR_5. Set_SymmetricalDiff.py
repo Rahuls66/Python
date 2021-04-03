8b78c17909c0ed805ebd9516b418b7a64bff5f96
Rahul Shah <rahul.shah4513@gmail.com>
2021-04-03 23:37:19 +0530

    Create HR_5. Set_SymmetricalDiff.py

---
+++ HR_5. Set_SymmetricalDiff.py
@@ -0,0 +1,18 @@
+#Set Symmetric Difference
+
+n = int(input())
+m = map(int, input().split())
+m = set(m)
+b = int(input())
+l = map(int, input().split())
+l = set(l)
+print(len(m.union(l) - m.intersection(l)))
+
+'''
+Expected Output:
+9
+1 2 3 4 5 6 7 8 9
+9
+10 1 2 3 11 21 55 6 8
+8
+''' 
