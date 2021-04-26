95e4fa0fb8e79cfc8f561b5f6da9764f8d55d7d4
Rahul Shah <rahul.shah4513@gmail.com>
2021-04-27 00:06:12 +0530

    Create 039. MultiplicativeTableWithWhileLoop.py

---
+++ 039. MultiplicativeTableWithWhileLoop.py
@@ -0,0 +1,56 @@
+# Program tofind Multiplciative table of the given numbers using While Loop (24, 25, 29)
+
+i = 0
+while i < 11:
+    print("24 X", str(i), "=", str(24*i))
+    i += 1
+print()
+i = 0    
+while i < 11:
+    print("50 X", str(i), "=", str(50*i))
+    i += 1
+print()
+i = 0    
+while i < 11:
+    print("29 X", str(i), "=", str(29*i))
+    i += 1
+print()
+
+'''
+Expected Output:
+24 X 0 = 0
+24 X 1 = 24
+24 X 2 = 48
+24 X 3 = 72
+24 X 4 = 96
+24 X 5 = 120
+24 X 6 = 144
+24 X 7 = 168
+24 X 8 = 192
+24 X 9 = 216
+24 X 10 = 240
+
+50 X 0 = 0
+50 X 1 = 50
+50 X 2 = 100
+50 X 3 = 150
+50 X 4 = 200
+50 X 5 = 250
+50 X 6 = 300
+50 X 7 = 350
+50 X 8 = 400
+50 X 9 = 450
+50 X 10 = 500
+
+29 X 0 = 0
+29 X 1 = 29
+29 X 2 = 58
+29 X 3 = 87
+29 X 4 = 116
+29 X 5 = 145
+29 X 6 = 174
+29 X 7 = 203
+29 X 8 = 232
+29 X 9 = 261
+29 X 10 = 290
+'''
