83982e5939403ff3e8004e74b46673dc64bc92d5
Rahul Shah <rahul.shah4513@gmail.com>
2021-04-29 23:42:28 +0530

    Update CoW_Odd_Count_From_List.py

--- CoW_Odd_Count_From_List.py
+++ CoW_Odd_Count_From_List.py
@@ -4,14 +4,17 @@ Given an array of integers, find the one that appears an odd number of times.
 There will always be only one integer that appears an odd number of times.
 '''
 
-def find_it(seq):
-    for i in seq:
-        if seq.count(i) % 2 != 0:    
-            return i
+def find_it(x):
+    l1 = []
+    for i in x:
+        if x.count(i) % 2 != 0:
+            if i not in l1:
+                l1.append(i)
+    return (l1) 
 
-find_it([1,2,3,4,5,1,3,3,4])
+    find_it([1,2,3,4,5,1,3,3,4])
 
 '''
 Expected Output:
-2
+[2, 3, 5]
 '''
