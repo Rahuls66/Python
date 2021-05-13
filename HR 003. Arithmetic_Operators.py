2c3ce7f2c75a1083c5b846d394b4ccd8d4fbbb66
Rahul Shah <rahul.shah4513@gmail.com>
2021-05-13 22:34:18 +0530

    Create HR 003. Arithmetic_Operators.py

---
+++ HR 003. Arithmetic_Operators.py
@@ -0,0 +1,21 @@
+#Arithmetic Operators
+
+a = int(input(""))
+b = int(input(""))
+
+sum = a + b
+diff = a - b
+prod = a * b
+
+print(sum)
+print(diff)
+print(prod)
+
+'''
+Expected Output:
+3
+2
+5
+1
+6
+'''
