defc2e5e5f413b0f2fc3dc72ee1adb517ee89970
Rahul Shah <rahul.shah4513@gmail.com>
2021-05-15 13:26:38 +0530

    Create HR 015. sWAP cASE.py

---
+++ HR 015. sWAP cASE.py
@@ -0,0 +1,11 @@
+# sWAP cASE
+
+def swap_case(s):
+    return s.swapcase()
+
+swap_case('HackerRank.com presents "Pythonist 2".')
+
+'''
+Expected Output:
+hACKERrANK.COM PRESENTS "pYTHONIST 2".
+'''
