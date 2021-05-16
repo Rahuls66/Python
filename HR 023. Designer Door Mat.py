c33822701f843287e5448d59b40275524399670f
Rahul Shah <rahul.shah4513@gmail.com>
2021-05-16 22:55:44 +0530

    Create HR 023. Designer Door Mat.py

---
+++ HR 023. Designer Door Mat.py
@@ -0,0 +1,20 @@
+# Designer Door Mat
+
+n, m = map(int,input().split())
+for i in range(1, n, 2): 
+    print((i * ".|.").center(m, "-"))
+print("WELCOME".center(m,"-"))
+for i in range(n-2,-1,-2): 
+    print((i * ".|.").center(m, "-"))
+    
+'''
+Expected Output:
+7 21
+---------.|.---------
+------.|..|..|.------
+---.|..|..|..|..|.---
+-------WELCOME-------
+---.|..|..|..|..|.---
+------.|..|..|.------
+---------.|.---------
+'''
