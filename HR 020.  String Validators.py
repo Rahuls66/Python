5e4024269c32794013217a2741f911a20c5fb826
Rahul Shah <rahul.shah4513@gmail.com>
2021-05-16 22:53:01 +0530

    Create HR 020.  String Validators.py

---
+++ HR 020.  String Validators.py
@@ -0,0 +1,19 @@
+# String Validators
+
+s = input()
+
+print(any(char.isalnum() for char in s))
+print(any(char.isalpha() for char in s))
+print(any(char.isdigit() for char in s))
+print(any(char.islower()  for char in s))
+print(any(char.isupper() for char in s))
+
+'''
+Expected Output:
+qA2
+True
+True
+True
+True
+True
+'''
