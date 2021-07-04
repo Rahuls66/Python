7f57bf23f133a639583e2a235caa7b8aaea09c4e
Rahul Shah <rahul.shah4513@gmail.com>
2021-07-04 21:19:46 +0530

    Create HR 049. Integers Come In All Sizes.py

---
+++ HR 049. Integers Come In All Sizes.py
@@ -0,0 +1,17 @@
+# Integers Come In All Sizes
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print(pow(9,29) + pow(7, 27))
+
+'''
+Expected Output:
+999
+999
+999
+999
+4710194409608608369201743232
+'''
