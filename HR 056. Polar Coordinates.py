4b13735f3cd41df6accec52c0b818c85e5db94ee
Rahul Shah <rahul.shah4513@gmail.com>
2021-07-04 21:48:16 +0530

    Create HR 056. Polar Coordinates.py

---
+++ HR 056. Polar Coordinates.py
@@ -0,0 +1,17 @@
+# Polar Coordinates
+
+import cmath
+
+s = complex(input())
+#s = complex(s)
+r = abs(s)
+ph = cmath.phase(s)
+print(r)
+print(ph)
+
+'''
+Expected Output:
+1+2j
+2.23606797749979
+1.1071487177940904
+'''
