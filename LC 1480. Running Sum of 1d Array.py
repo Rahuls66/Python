f2063206c05a6f73edac9f901fd44891a27bd4a1
Rahul Shah <rahul.shah4513@gmail.com>
2021-07-15 03:49:58 +0530

    Create LC 1480. Running Sum of 1d Array.py

---
+++ LC 1480. Running Sum of 1d Array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        sum = 0
+        res = []
+        for i in range(len(nums)):
+            sum += nums[i]
+            res.append(sum)
+        return res
+      
+'''
+[1,3,6,10]
+
+'''
